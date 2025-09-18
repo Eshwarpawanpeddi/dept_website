8f95e143b969ced9d0f2efd321108f286c05eb6a
Eshwar pawan peddi <eshwarpavanpeddi@gmail.com>
2025-09-18 13:38:08 +0000

    update

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const nextButton = document.querySelector('.next-btn');
 const video = document.querySelector('.hero-video');
-const movieList = ['videos/mainvid.mp4', 'videos/hero-1.mp4', 'videos/hero-2.mp4', 'videos/hero-3.mp4', 'videos/hero-4.mp4'];
-let index = 0;
 
+const movieList = ['videos/mainvid.mp4', 'videos/hero-1.mp4', 'videos/hero-2.mp4', 'videos/hero-3.mp4', 'videos/hero-4.mp4'];
+
+let index = 0;
 nextButton.addEventListener('click', function() {
-    index = (index + 1) % movieList.length; // This line ensures the video list loops
+    index = (index + 1) % movieList.length; 
     video.src = movieList[index];
 });
