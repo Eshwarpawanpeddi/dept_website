48b3605fba29f041c6a6ae7b2c1f5b87904dacdb
Eshwar pawan peddi <eshwarpavanpeddi@gmail.com>
2025-09-18 13:05:11 +0000

    fixed commits

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 const nextButton = document.querySelector('.next-btn');
 const video = document.querySelector('.hero-video');
 
-const movieList = ['videos/hero-1.mp4', 'videos/hero-2.mp4', 'videos/hero-3.mp4', 'videos/hero-4.mp4'];
+// Add the new video to the beginning of the list
+const movieList = ['videos/mainvid.mp4', 'videos/hero-1.mp4', 'videos/hero-2.mp4', 'videos/hero-3.mp4', 'videos/hero-4.mp4'];
 
 let index = 0;
 nextButton.addEventListener('click', function() {
-    index += 1;
+    index = (index + 1) % movieList.length;
     video.src = movieList[index];
-
-    if (index === 3) {
-        index = -1;
-    }
-})
+});
